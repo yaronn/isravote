dab167b3388f289338dff309502fafb34a995a7c
Yaron Naveh <yaron.naveh@hp.com>
2012-12-14 23:35:52 +0200

    ui

--- votes.js
+++ votes.js
@@ -1,14 +1,14 @@
 
 var parties = [
-  {name: 'labour', pages: [{name: 'shelly', id: 162319074217}]},
-  {name: 'atid', pages: [{name: 'lapid', id: 107836625941364}]},
-  {name: 'meretz', pages: [{name: 'zehava', id: 115028251920872}]},
-  {name: 'hatnua', pages: [{name: 'tzipi', id: 37665519437}]},
-  {name: 'likud beitenu', pages: [{name: 'bibi', id: 268108602075}, {name: 'ivet', id: 178433145502975}]},
-  {name: 'mafdal', pages: [{name: 'benet', id: 396697410351933}]},
-  {name: 'shas', pages: [{name: 'ishai', id: 137409609701165}]},
-  {name: 'hadash', pages: [{name: 'khenin', id: 295104730523499}]},
-  {name: 'otzma', pages: [{name: 'eldad', id: 149782518386564}]}
+  {name: 'העבודה', side: 'left', pages: [{name: "שלי יחימוביץ'", id: 162319074217}]},
+  {name: 'יש עתיד', side: 'left', pages: [{name: 'יאיר לפיד', id: 107836625941364}]},
+  {name: 'מרצ', side: 'left', pages: [{name: 'זהבה גלאון', id: 115028251920872}]},
+  {name: 'התנועה', side: 'left', pages: [{name: 'ציפי לבני', id: 37665519437}, {name: 'עמיר פרץ', id: 154570404606299}]},
+  {name: 'הליכוד ביתנו', side: 'right', pages: [{name: 'בנימין נתניהו', id: 268108602075}, {name: 'אביגדור ליברמן', id: 178433145502975}]},
+  {name: 'הבית היהודי-האיחוד הלאומי', side: 'right',pages: [{name: 'נפתלי בנט', id: 396697410351933}]},
+  {name: 'ש"ס', side: 'right', pages: [{name: 'אלי ישי', id: 137409609701165}]},
+  {name: 'חד"ש', side: 'left', pages: [{name: 'דב חנין', id: 295104730523499}]},
+  {name: 'עוצמה לישראל', side: 'right', pages: [{name: 'אריה אלדד', id: 149782518386564}]}
 ]
 
 function showVotes()
@@ -16,6 +16,27 @@ function showVotes()
   getVotesData(function(data) {
     showVotesData(data)
   })
+
+  buildLeftRight()
+}
+
+function buildLeftRight() {
+  for (var p in parties) {    
+    var party = parties[p]
+    var div = $(party.side=="left"?"#left":"#right")    
+
+    for (var s in party.pages) {
+      var person = party.pages[s]
+      div.append("<h3>חברים שאוהבים את "+person.name+":</h3>")
+      //div.append('<iframe src="//www.facebook.com/plugins/facepile.php?href=http%3A%2F%2Fwww.facebook.com%2F'+person.id+'&amp;action&amp;size=large&amp;max_rows=5&amp;width=500&amp;colorscheme=light&amp;appId=123092357853018" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:340px;" allowTransparency="true"></iframe>');      
+
+      div.append('<fb:facepile href="http://www.facebook.com/'+person.id+'" size="large" max_rows="4" width="400"></fb:facepile>')
+      div.append('<br />')
+    } 
+
+  }
+
+  FB.XFBML.parse();   
 }
 
 function showVotesData(data) {
@@ -23,7 +44,7 @@ function showVotesData(data) {
   
   var colors = Highcharts.getOptions().colors;
   var categories = []
-  parties.forEach(function(item) {
+  data.forEach(function(item) {
     categories.push(item.name)
   })
 
@@ -48,10 +69,10 @@ function showVotesData(data) {
       type: 'column'
     },
     title: {
-      text: '2013 Friends Election Restuls'
+      text: 'סקר בחירות 2013'
     },
     subtitle: {
-      text: ''
+      text: 'למי החברים שלך עשו לייק?'
     },
     xAxis: {
       categories: categories
@@ -93,10 +114,11 @@ function makeResultsArray(hash) {
   var res = [];
 
   for (var p in hash)    
-    res.push({y: hash[p]}) 
+    res.push({name: p, y: hash[p]}) 
 
+  
   res.sort(function(a,b) {
-    return a[1]-b[1]
+    return a.y-b.y
   })
 
   return res;
